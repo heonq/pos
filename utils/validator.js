f8c6ae103f65a41cef0c0400d16edc3cf54291c5
heonq <gkagjsrb@naver.com>
2024-03-18 11:55:31 +0900

    fix: 유효성 검사 기능 오류 수정
    - 이름 문자열의 맨 앞과 맨 뒤에 공백이 있는지 확인하는 정규표현식 수정:
    /^\s*|\s*$/ -> /^\s+|\s+$/

--- utils/validator.js
+++ utils/validator.js
@@ -141,7 +141,7 @@ const validator = {
   },
 
   validateLastStringBlank(names) {
-    const regex = /^\s*|\s*$/;
+    const regex = /^\s+|\s+$/;
     if (names.some((name) => regex.test(name))) {
       alert('이름의 첫번째와 마지막 글자는 공백으로 설정할 수 없습니다.');
       return false;
