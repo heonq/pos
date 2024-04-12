d0d46b26c0c3a8707912694d6b4445587107cdc2
heonq <gkagjsrb@naver.com>
2024-04-12 18:07:44 +0900

    chore: webpack output path 변경

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist/js'),
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   resolve: {
